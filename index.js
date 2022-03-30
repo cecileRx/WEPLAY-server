1454dc1b82087444f27b3260b6afafa6bdc1019b
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2022-03-30 10:43:12 +0100

    sql connection set + db insertion test

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'UserSystem',
+});
+
+app.post('/create', (req, res) => {
+    const info = req.body.info;
+
+    db.query(
+      'INSERT INTO users (info) VALUES (?)',
+      [info],
+      (err, res) => {
+        if (err) => {
+          console.log(err)
+        } else {
+          res.send('Values Inserted')
+        }
+      }
+    );
+});
+
 
 app.listen(3001, () => {
-  console.log('yeh is server is running on port 3001')
+  console.log('yeh the server is running on port 3001')
 })
 
